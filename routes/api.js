e1a41022110f0b0d435daa13b19ff49328939f29
Cătălin Anghel-Ursu <cat.ang.dev@gmail.com>
2019-05-21 17:12:55 +0300

    Update api.js
    Included all the situations when the number or unit is invalid.
    Added the POST method for the situation when the input field is empty to respond with an "invalid unit" error.

--- routes/api.js
+++ routes/api.js
@@ -8,28 +8,43 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require('chai').expect;
+const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
   
-  var convertHandler = new ConvertHandler();
+  const convertHandler = new ConvertHandler();  
   
-  app
-    .route('/api/convert')
-    .get(function (req, res){
-    var input = req.query.input;
-    var initNum = convertHandler.getNum(input);
-    var initUnit = convertHandler.getUnit(input);
-    var returnNum = convertHandler.convert(initNum, initUnit);
-    var returnUnit = convertHandler.getReturnUnit(initUnit);
-    var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+  app.get('/api/convert', (req, res) => {
+    const invalNum  = "invalid number";
+    const invalUnit = "invalid unit";
+    const invalNumAndUnit = "invalid number and unit";
     
-    returnUnit == "invalid unit"   ?  res.json("invalid unit")       :
-    returnNum  == "invalid number" ?  res.json("invalid number")     :     
-    returnUnit == "invalid unit"   && returnNum  == "invalid number" ? res.json("invalid number and unit") :
+    const input = req.query.input;    
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);    
+    
+    /*console.log("Input is: "  + input);
+    console.log("initNum is: " + initNum);
+    console.log("initUnit is: " + initUnit);
+    console.log("returnNum is: " + returnNum);
+    console.log("returnUnit is: " + returnUnit);
+    console.log("toString is: "    + toString);*/
+    
+    returnNum  == invalNum  && returnUnit == invalUnit ? res.status(200).send(invalNumAndUnit) :
+    initNum    == invalNum  || returnNum  == invalNum  ? res.status(200).send(invalNum)        :
+    initUnit   == invalUnit || returnUnit == invalUnit ? res.status(200).send(invalUnit)       :
     res.json({initNum: initNum, initUnit: initUnit, returnNum: returnNum, returnUnit: returnUnit, string: toString});
   
   });
+  
+  app.post('/api/convert', (req, res) => {
+    const input = req.body.input;
+    const invalUnit = "invalid unit";
     
+    !input ? res.json(invalUnit) : null;
+  });
 };
